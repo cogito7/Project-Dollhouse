784fe8f58f3c60366d0b246673096de482c5bb74
cogito7 <galindom808@gmail.com>
2025-04-21 16:52:09 -0400

    fixed camera switch now that camera rotates

--- Assets/Scenes/Scripts/CameraModality/CameraSwitcher.cs
+++ Assets/Scenes/Scripts/CameraModality/CameraSwitcher.cs
@@ -4,6 +4,7 @@ public class CameraSwitcher : MonoBehaviour
 {
 
     public GameObject[] cameras; // stores 2 cameras
+    public GameObject[] players; //stores 2 players
     private int currentCameraIndex = 0; // Tracks the current camera
     void Start()
     {
@@ -11,6 +12,11 @@ public class CameraSwitcher : MonoBehaviour
         foreach (var camera in cameras)
         {
             camera.SetActive(false);
+        }
+        //Make sure to deactivate all players initially
+        foreach(var player in players)
+        {
+            player.SetActive(false);
         }
 
         // Activate the first camera at the start
@@ -31,6 +37,7 @@ public class CameraSwitcher : MonoBehaviour
         for (int i = 0; i < cameras.Length; i++)
         {
             cameras[i].SetActive(i == cameraIndex);
+            players[i].SetActive(i == cameraIndex);
         }
     }
 }
