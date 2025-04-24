8e291ece28f56e98609895007c9065274ed34abf
Jelios04 <jadyn.englett@gmail.com>
2025-04-24 18:13:27 -0400

    UI Setup

--- Assets/Scenes/Scripts/CraftingModality/CraftingUIManager.cs
+++ Assets/Scenes/Scripts/CraftingModality/CraftingUIManager.cs
@@ -4,12 +4,15 @@ using UnityEngine.EventSystems;
 public class CraftingUIManager : MonoBehaviour
 {
     public GameObject craftingPanel;
+    public GameObject pausemenu;
     private bool isOpen = false;
     public static bool IsCraftingOpen { get; private set; }
+    public static bool IsPauseOpen { get; private set; }
 
     void Start()
     {
         craftingPanel.SetActive(false); // Hide the panel at start
+        pausemenu.SetActive(false); // Hide the panel at start
         Cursor.lockState = CursorLockMode.None; // Make sure cursor is always free
         Cursor.visible = true; // Ensure the cursor is visible from the start
     }
@@ -20,11 +23,26 @@ public class CraftingUIManager : MonoBehaviour
         IsCraftingOpen = isOpen;
         craftingPanel.SetActive(isOpen);
     }
+
     public void CloseCraftingUI()
     {
         isOpen = false;
         IsCraftingOpen = false;
         craftingPanel.SetActive(false);
+    }
+
+    public void TogglePauseUI()
+    {
+        isOpen = !isOpen;
+        IsPauseOpen = isOpen;
+        pausemenu.SetActive(isOpen);
+    }
+
+    public void ClosePauseUI()
+    {
+        isOpen = false;
+        IsPauseOpen = false;
+        pausemenu.SetActive(false);
     }
 
     void Update()
