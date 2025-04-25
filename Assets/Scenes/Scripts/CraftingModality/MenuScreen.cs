401e0d8baa70245261d8e02910de1995e091b994
Jelios04 <jadyn.englett@gmail.com>
2025-04-24 21:21:27 -0400

    Main Menu Code!
    We have a working main menu!!

---
+++ Assets/Scenes/Scripts/CraftingModality/MenuScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScreen : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
